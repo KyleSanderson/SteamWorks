1b00874b0ceecd06b3356af3b570315bc010ba29
David Anderson <dvander@alliedmods.net>
2020-08-22 16:12:49 -0700

    Upgrade to AMBuild 2.2.

--- configure.py
+++ configure.py
@@ -12,22 +12,27 @@ except:
 		sys.stderr.write('http://www.alliedmods.net/ambuild\n')
 	sys.exit(1)
 
-run = run.PrepareBuild(sourcePath=sys.path[0])
-run.default_build_folder = 'obj-' + run.target_platform
-run.options.add_option('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
-		                   help='Root search folder for HL2SDKs')
-run.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
-                       help='Path to Metamod:Source')
-run.options.add_option('--sm-path', type=str, dest='sm_path', default=None,
-                       help='Path to Sourcemod')
-run.options.add_option('--steamworks-path', type=str, dest='steamworks_path', default=None,
-                       help='Path to SteamWorks')
-run.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
-                       help='Enable debugging symbols')
-run.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
-                       help='Enable optimization')
-run.options.add_option('-s', '--sdks', default='all', dest='sdks',
-                       help='Build against specified SDKs; valid args are "all", "present", or '
-                            'comma-delimited list of engine names (default: %default)')
+# Hack to show a decent upgrade message, which wasn't done until 2.2.
+ambuild_version = getattr(run, 'CURRENT_API', '2.1')
+if ambuild_version.startswith('2.1'):
+	sys.stderr.write("AMBuild 2.2 or higher is required; please update\n")
+	sys.exit(1)
 
+run = run.BuildParser(sourcePath=sys.path[0], api='2.2')
+run.options.add_argument('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
+		                   help='Root search folder for HL2SDKs')
+run.options.add_argument('--mms-path', type=str, dest='mms_path', default=None,
+                       help='Path to Metamod:Source')
+run.options.add_argument('--sm-path', type=str, dest='sm_path', default=None,
+                       help='Path to Sourcemod')
+run.options.add_argument('--steamworks-path', type=str, dest='steamworks_path', default=None,
+                       help='Path to SteamWorks')
+run.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
+                       help='Enable debugging symbols')
+run.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
+                       help='Enable optimization')
+run.options.add_argument('-s', '--sdks', default='all', dest='sdks',
+                       help='Build against specified SDKs; valid args are "all", "present", or '
+                            'comma-delimited list of engine names')
+run.options.add_argument('--target', default=None, help='Override the default build target')
 run.Configure()
